e29f8a1b688f6c4c60a46a0c4b0dc5cd5421e70a
~ Yurii <locomoc.yt@gmail.com>
2025-06-26 17:57:02 +0300

    tt1

--- src/main/java/com/polis/Main.java
+++ src/main/java/com/polis/Main.java
@@ -1,17 +1,36 @@
 package com.polis;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.IOException;
+import java.util.Objects;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/base_layout.fxml"));
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+        Scene scene = new Scene(root);
+//        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/fxml/style.css")).toExternalForm());
+
+        primaryStage.setTitle("Casino Client");
+        primaryStage.setWidth(1100);
+        primaryStage.setHeight(600);
+        primaryStage.setResizable(true);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+
 }
---
+++ src/main/java/com/polis/controller/RegistrationController.java
@@ -0,0 +1,7 @@
+package com.polis.controller;
+
+import javafx.scene.image.ImageView;
+
+public class RegistrationController {
+    public ImageView headerImage;
+}
